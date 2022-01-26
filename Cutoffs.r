519a23a5dd600ddfa9a1f03243d37d9a2540a380
RadiSadek <60088784+RadiSadek@users.noreply.github.com>
2022-01-26 13:12:40 +0200

    recorrect distribution cutoff for application

--- Cutoffs.r
+++ Cutoffs.r
@@ -5,7 +5,7 @@
 ################################################################
 
 # Application Flexcredit
-cu_app_flexcredit <- c(0.6,0.475,0.375,0.3,0.225)
+cu_app_flexcredit <- c(0.55,0.475,0.375,0.3,0.225)
 
 
 # Repeat Flexcredit
