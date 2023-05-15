7398ec79c6a2271086db0040c51159c4dc5b83ef
RadiSadek <60088784+RadiSadek@users.noreply.github.com>
2023-05-15 15:18:35 +0300

    increase cutoffs for repeat bad and worst offices

--- Cutoffs.r
+++ Cutoffs.r
@@ -13,5 +13,5 @@ cu_app_flexcredit_worst <- c(0.525,0.525,0.4,0.3,0.2)
 # Repeat Flexcredit
 cu_beh_flexcredit <- c(0.6,0.4,0.3,0.22,0.13)
 cu_beh_flexcredit_good <- c(0.7,0.4,0.3,0.22,0.13)
-cu_beh_flexcredit_bad <- c(0.5,0.4,0.3,0.22,0.13)
-cu_beh_flexcredit_worst <- c(0.4,0.4,0.3,0.22,0.13)
+cu_beh_flexcredit_bad <- c(0.6,0.4,0.3,0.22,0.13)
+cu_beh_flexcredit_worst <- c(0.5,0.4,0.3,0.22,0.13)
