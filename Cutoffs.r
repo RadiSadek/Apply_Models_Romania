0371e73e4d3625494e96cbaa301690c5957a5dcd
syuleya-alieva <suealieva99@gmail.com>
2023-11-17 10:24:16 +0200

    Reverted the cutoffs to the old ones.

--- Cutoffs.r
+++ Cutoffs.r
@@ -11,8 +11,7 @@ cu_app_flexcredit_bad <- c(0.575,0.525,0.4,0.3,0.2)
 cu_app_flexcredit_worst <- c(0.525,0.525,0.4,0.3,0.2)
 
 # Repeat Flexcredit
-cu_beh_flexcredit <- c(0.5,0.35, 0.23, 0.15, 0.09)
-cu_beh_flexcredit_good <- c(0.55,0.35, 0.23, 0.15, 0.09)
-cu_beh_flexcredit_bad <- c(0.5,0.35, 0.23, 0.15, 0.09)
-cu_beh_flexcredit_worst <- c(0.45,0.35, 0.23, 0.15, 0.09)
-
+cu_beh_flexcredit <- c(0.6,0.4,0.3,0.22,0.13)
+cu_beh_flexcredit_good <- c(0.7,0.4,0.3,0.22,0.13)
+cu_beh_flexcredit_bad <- c(0.6,0.4,0.3,0.22,0.13)
+cu_beh_flexcredit_worst <- c(0.5,0.4,0.3,0.22,0.13)
